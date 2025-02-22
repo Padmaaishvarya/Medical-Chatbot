fc61ae96fe1e730031391098622b7a4a63ac90c1
Padmaaishvarya <aishvaryasakthivel2004@gmail.com>
2025-02-22 19:17:48 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Medical-Chatbot
+
+
+# How to run?
+### STEPS:
+
+Clone the reppository
+
+'''bash
+Project repo: https://github.com/
+'''
+### STEP 01- Create a conda 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aishu',
+    author_email= 'aishvaryasakthivel@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
